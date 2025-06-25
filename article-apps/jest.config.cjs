2717c30d9b887bc1eae7ddbc3b37a1ac2d25317a
nurcahyoprasetiyo <nurcahyoprasetiyo4@gmail.com>
2025-06-25 13:58:55 +0700

    fix: unit test

--- article-apps/jest.config.cjs
+++ article-apps/jest.config.cjs
@@ -1,19 +1,16 @@
 module.exports = {
-  preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy",
   },
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.app.json",
+      },
+    ],
   },
   testMatch: ["**/__tests__/**/*.test.ts?(x)"],
-
-  // ✅ Tambahan penting untuk menunjuk tsconfig yang cocok untuk test
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.app.json", // ganti sesuai lokasi tsconfig untuk app (bukan tsconfig.json root)
-    },
-  },
 };
